a3f747a882f66794e17db219d0e23dd52689a035
Fajrul <perfectratio1509@gmail.com>
2025-02-04 07:24:48 +0700

    Update __init__.py

--- ocr/preprocessors/__init__.py
+++ ocr/preprocessors/__init__.py
@@ -3,11 +3,15 @@ from .adaptive_thresholding import apply_adaptive_thresholding
 from .deskewing import deskew_image
 from .upscaling import upscale_image
 from .crop_letterhead import crop_letterhead
+from .background_removal import remove_background
+from .contrast_enhancement import enhance_contrast
 
 __all__ = [
     "denoise_image",
     "apply_adaptive_thresholding",
     "deskew_image",
     "upscale_image",
-    "crop_letterhead"
+    "crop_letterhead",
+    "remove_background",
+    "enhance_contrast"
 ]
